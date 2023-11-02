cfe1ed106e62dc1478923c22c90d7d185b0cc33b
ignaciomtp <ignaciomtp@gmail.com>
2023-11-02 10:20:36 +0100

    Travels public endpoint with pagination

---
+++ app/Http/Controllers/Api/V1/TravelController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Travel;
+use App\Http\Resources\TravelResource;
+
+class TravelController extends Controller
+{
+    public function index() {
+        $travels = Travel::where('is_public', false)->paginate();
+        return TravelResource::collection($travels);
+    }
+}
